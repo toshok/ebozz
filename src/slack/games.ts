1e185c461af036af58c7dd7dfccdaf5ab3cdd969
Chris Toshok <toshok@hound.sh>
2023-04-20 19:28:58 -0700

    add a couple more games

--- src/slack/games.ts
+++ src/slack/games.ts
@@ -11,6 +11,10 @@ const GAMES: Record<string, { name: string; path: string }> = {
     name: "Zork III: The Dungeon Master",
     path: "./gamefiles/zork3-r25-s860811.z3",
   },
+  lgop: {
+    name: "Leather Goddesses of Phobos",
+    path: "./gamefiles/leathergoddesses-r59-s860730.z3",
+  },
   hitchhikers: {
     name: "The Hitchhiker's Guide To The Galaxy",
     path: "./gamefiles/hitchhiker-r59-s851108.z3",
@@ -19,13 +23,17 @@ const GAMES: Record<string, { name: string; path: string }> = {
     name: "Spellbreaker",
     path: "./gamefiles/spellbreaker-r87-s860904.z3",
   },
-  wishbringer: {
-    name: "Wishbringer",
-    path: "./gamefiles/wishbringer-r69-s850920.z3",
+  stationfall: {
+    name: "Stationfall",
+    path: "./gamefiles/stationfall-r107-s870430.z3",
   },
   trinity: {
     name: "Trinity (doesn't work currently)",
     path: "./gamefiles/trinity-r15-s870628.z4",
+  },
+  wishbringer: {
+    name: "Wishbringer",
+    path: "./gamefiles/wishbringer-r69-s850920.z3",
   },
 } as const;
 
