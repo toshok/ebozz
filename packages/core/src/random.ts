5b7c3576784edcf1433dce36e14765d3b84653de
Chris Toshok <toshok@hound.sh>
2023-05-04 08:53:07 -0700

    one step closer to packages/building

--- packages/core/src/random.ts
+++ packages/core/src/random.ts
@@ -1,9 +1,9 @@
 import seedrandom from "seedrandom";
 
-let rng: () => number = new seedrandom.alea({ entropy: true });
+let rng: () => number = seedrandom("", { entropy: true });
 
 export function randomSeed(seed: string) {
-  rng = new seedrandom.alea(seed);
+  rng = seedrandom.alea(seed);
 }
 
 export function randomInt(range: number): number {
